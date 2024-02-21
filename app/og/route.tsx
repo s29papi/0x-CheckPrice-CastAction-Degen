2dcd76e9e8be0ef4a9d398f0453e30f1d2e359f4
S29 <nots29vv@gmail.com>
2024-02-21 23:16:14 +0100

    First frame

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -8,8 +8,42 @@ export const runtime = 'edge';
  
 export async function GET(req: NextRequest) {}
 
-
-
-
-
-
+  const fetchData = async (domain: string) => {
+    let status;
+    let firstYearRegistrationFee;
+    let renewalFee;
+    let domainName;
+    
+    try {
+      // Check if domain name contains a dot
+      if (!domain.includes('.')) {
+        // If not, concatenate ".com"
+        domain += '.com';
+      }
+      
+      const response = await fetch(`https://api.3dns.xyz/api/v1/core_backend_service/domain/check_domain/${domain}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      status = data.status;
+      firstYearRegistrationFee = data.price.CURRENCY_USD.firstYearRegistrationFee;
+      renewalFee = data.price.CURRENCY_USD.renewalFee;
+      domainName = data.domain;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  
+    return { status, firstYearRegistrationFee, renewalFee, domainName };
+  };
+  
+  function addDecimalBeforeLastTwoZeros(str: string) {
+    let num = parseFloat(str);
+    num /= 100;
+    let result = num.toFixed(2);
+  
+    return result;
+  }
+  
+  
+  
