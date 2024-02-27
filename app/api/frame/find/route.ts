3d51d3acb5b2bff1bc73e48d86efa11c657397bb
S29 <nots29vv@gmail.com>
2024-02-27 03:16:39 +0100

    updated og meta data

--- app/api/frame/find/route.ts
+++ app/api/frame/find/route.ts
@@ -4,10 +4,17 @@ import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
 const body: FrameRequest = await req.json();
-let domainName;
+let domainName: string = body.untrustedData.inputText;
 
-if (body.untrustedData.inputText) {
-    domainName = body.untrustedData.inputText
+let valid = await fetchData(domainName) 
+if (!valid) {
+    return new NextResponse(`<!DOCTYPE html><html><head>
+            <title>Success Page</title>
+            <meta property="fc:frame" content="vNext" />
+            <meta property="fc:frame:image" content="https://3dns-domain-register-frame.vercel.app/og?domainName=${domainName}"/>
+            <meta property="fc:frame:button:1" content="Back" />
+            <meta property="fc:frame:post_url" content="https://3dns-domain-register-frame.vercel.app/api/frame/process?domainName=${domainName}"/>
+            </head></html>`);
 }
 
 return new NextResponse(`<!DOCTYPE html><html><head>
@@ -27,3 +34,30 @@ export async function POST(req: NextRequest): Promise<Response> {
 }
 
 export const dynamic = 'force-dynamic';
+
+
+const fetchData = async (domain: string) => {
+    let status;
+
+    
+    try {
+      // Check if domain name contains a dot
+      if (!domain.includes('.')) {
+        // If not, concatenate ".com"
+        domain += '.com';
+      }
+      
+      const response = await fetch(`https://api.3dns.xyz/api/v1/core_backend_service/domain/check_domain/${domain}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      status = data.status;
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  
+    return status;
+  };
+  
--- app/api/frame/process/route.ts
+++ app/api/frame/process/route.ts
@@ -4,13 +4,12 @@ import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
 const body: FrameRequest = await req.json();
+const searchParams = req.nextUrl.searchParams;
+const domainName:any = searchParams.get("domainName");
 let buttonsIdx;
 
-if (body.untrustedData.buttonIndex) {
-    buttonsIdx = body.untrustedData.buttonIndex
-}
+if (body.untrustedData.buttonIndex) buttonsIdx = body.untrustedData.buttonIndex
 
-// code to check success
 
 if (buttonsIdx == 1) {
     return  new NextResponse(`<!DOCTYPE html><html><head>
@@ -42,3 +41,29 @@ export async function POST(req: NextRequest): Promise<Response> {
 }
 
 export const dynamic = 'force-dynamic';
+
+const fetchData = async (domain: string) => {
+    let status;
+
+    
+    try {
+      // Check if domain name contains a dot
+      if (!domain.includes('.')) {
+        // If not, concatenate ".com"
+        domain += '.com';
+      }
+      
+      const response = await fetch(`https://api.3dns.xyz/api/v1/core_backend_service/domain/check_domain/${domain}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      status = data.status;
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  
+    return status;
+  };
+  
