dfba7886890627045ee2d963974fc367ccfdef22
S29 <nots29vv@gmail.com>
2024-02-27 02:42:59 +0100

    updated og meta data

--- app/api/frame/find/route.ts
+++ app/api/frame/find/route.ts
@@ -14,8 +14,10 @@ return new NextResponse(`<!DOCTYPE html><html><head>
   <title>Success Page</title>
   <meta property="fc:frame" content="vNext" />
   <meta property="fc:frame:image" content="https://3dns-domain-register-frame.vercel.app/og?domainName=${domainName}"/>
-  <meta property="fc:frame:button:1" content="Complete" />
+  <meta property="fc:frame:button:1" content="Back" />
   <meta property="fc:frame:button:1:action" content="post"/>
+  <meta property="fc:frame:button:2" content="Purchase" />
+  <meta property="fc:frame:button:2:action" content="post_redirect"/>
   <meta property="fc:frame:post_url" content=""/>
   </head></html>`);
 }
