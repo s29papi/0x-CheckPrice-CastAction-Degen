7c3ca9c4b2e49b3ad93ecb9e1a9c5328d8345210
S29 <nots29vv@gmail.com>
2024-02-27 02:10:01 +0100

    updated og meta data

--- app/api/frame/find/route.ts
+++ app/api/frame/find/route.ts
@@ -5,17 +5,15 @@ import { NextRequest, NextResponse } from 'next/server';
 async function getResponse(req: NextRequest): Promise<NextResponse> {
 const body: FrameRequest = await req.json();
 
-const { isValid, message } = await getFrameMessage(body, {
-    allowFramegear: true, 
-  });
+const { isValid, message } = await getFrameMessage(body);
 if (message?.input) {
     throw new Error("message input is undefined")
 }
 return new NextResponse(`<!DOCTYPE html><html><head>
   <title>Success Page</title>
-  <meta property="og:image" content="http://localhost:3002/base.png?valid=${isValid}"/>
+  <meta property="og:image" content="https://3dns-domain-register-frame.vercel.app/base.png?valid=${isValid}"/>
   <meta property="fc:frame" content="vNext" />
-  <meta property="fc:frame:image" content="http://localhost:3002/base.png?fid=${message?.input}"/>
+  <meta property="fc:frame:image" content="https://3dns-domain-register-frame.vercel.app/base.png?fid=${message?.input}"/>
   <meta property="fc:frame:button:1" content="Complete" />
   <meta property="fc:frame:button:1:action" content="post"/>
   <meta property="fc:frame:post_url" content=""/>
