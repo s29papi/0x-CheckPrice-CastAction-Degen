d3558d3bb9a398ba1104667756d832aecdf885a1
S29 <nots29vv@gmail.com>
2024-02-27 02:12:00 +0100

    updated og meta data

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -10,7 +10,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   <meta property="fc:frame" content="vNext" />
   <meta property="og:image" content="https://3dns-domain-register-frame.vercel.app/search-rescale.png"/>
   <meta property="fc:frame:image" content="https://3dns-domain-register-frame.vercel.app/search-rescale.png"/>
-  <meta property="fc:frame:input:text" content="Enter Domain ( Deafult: .com )" />
+  <meta property="fc:frame:input:text" content="Enter a Domain ( Deafult: .com )" />
   <meta property="fc:frame:button:1" content="Search" />
   <meta property="fc:frame:button:1:action" content="post"/>
   <meta property="fc:frame:post_url" content="https://3dns-domain-register-frame.vercel.app/api/frame/find"/>
