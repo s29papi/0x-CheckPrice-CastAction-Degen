007d9748396c13d5fac3980d2d18891f80c44852
S29 <nots29vv@gmail.com>
2024-02-27 03:26:34 +0100

    updated og meta data

--- app/api/frame/find/route.ts
+++ app/api/frame/find/route.ts
@@ -12,7 +12,7 @@ if (valid == "STATUS_UNAVAILABLE") {
             <title>Success Page</title>
             <meta property="fc:frame" content="vNext" />
             <meta property="fc:frame:image" content="https://3dns-domain-register-frame.vercel.app/og?domainName=${domainName}"/>
-            <meta property="fc:frame:button:1" content="Back ${valid}" />
+            <meta property="fc:frame:button:1" content="Back" />
             <meta property="fc:frame:button:1:action" content="post"/>
             <meta property="fc:frame:post_url" content="https://3dns-domain-register-frame.vercel.app/api/frame/process?domainName=${domainName}"/>
             </head></html>`);
@@ -22,7 +22,7 @@ return new NextResponse(`<!DOCTYPE html><html><head>
   <title>Success Page</title>
   <meta property="fc:frame" content="vNext" />
   <meta property="fc:frame:image" content="https://3dns-domain-register-frame.vercel.app/og?domainName=${domainName}"/>
-  <meta property="fc:frame:button:1" content="Back ${valid}" />
+  <meta property="fc:frame:button:1" content="Back" />
   <meta property="fc:frame:button:1:action" content="post"/>
   <meta property="fc:frame:button:2" content="Purchase" />
   <meta property="fc:frame:button:2:action" content="post_redirect"/>
