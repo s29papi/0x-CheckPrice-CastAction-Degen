be54dc72e1a32f19ff4e1296cb1e7f87ff18c72a
S29 <nots29vv@gmail.com>
2024-02-27 02:39:46 +0100

    updated og meta data

--- app/api/frame/find/route.ts
+++ app/api/frame/find/route.ts
@@ -4,14 +4,16 @@ import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
 const body: FrameRequest = await req.json();
+let domainName;
 
-if (body) {
-    throw new Error("Error resolving body");
+if (body.untrustedData.inputText) {
+    domainName = body.untrustedData.inputText
 }
+
 return new NextResponse(`<!DOCTYPE html><html><head>
   <title>Success Page</title>
   <meta property="fc:frame" content="vNext" />
-  <meta property="fc:frame:image" content="https://3dns-domain-register-frame.vercel.app/base.png?fid=${body.untrustedData.inputText}"/>
+  <meta property="fc:frame:image" content="https://3dns-domain-register-frame.vercel.app/og?domainName=${domainName}"/>
   <meta property="fc:frame:button:1" content="Complete" />
   <meta property="fc:frame:button:1:action" content="post"/>
   <meta property="fc:frame:post_url" content=""/>
