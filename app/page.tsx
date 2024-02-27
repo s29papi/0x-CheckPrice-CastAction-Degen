6297b232bf503c3c6bc79fa6fde9983108baf63f
S29 <nots29vv@gmail.com>
2024-02-27 04:36:04 +0100

    updated og meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-'use client';
-import {useEffect} from "react";
-import {useRouter} from "next/navigation";
+
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
@@ -28,15 +26,12 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const router = useRouter();
-  useEffect(() => {
     const redirectUrl = `https://app.3dns.box/search`;
 
     // Perform the redirect
     window.location.href = redirectUrl; // For a full page reload redirect
     // Or use Next.js router for client-side redirect (comment out the line above if using this)
     // router.push(youtubeUrl);
-}, [router]);
   return (
     <>
      
