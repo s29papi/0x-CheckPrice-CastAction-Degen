a2650b6bf376ecbb7924e8a19c47f461c68bc9c1
S29 <nots29vv@gmail.com>
2024-02-28 21:03:57 +0100

    changed the page.tsx to redirect to https://app.3dns.box

--- app/api/frame/find/route.ts
+++ app/api/frame/find/route.ts
@@ -11,12 +11,12 @@ if (data.timeOut) {
 
 }
 
-let imageUrl = "https://3dns-domain-register-frame.vercel.app/og?domainName=${data.domainName}&&renewalFee=${data.renewalFee}&&firstYearRegistrationFee=${data.firstYearRegistrationFee}&&status=${data.status}"
+let imageUrl = `https://3dns-domain-register-frame.vercel.app/og?domainName=${data.domainName}&&renewalFee=${data.renewalFee}&&firstYearRegistrationFee=${data.firstYearRegistrationFee}&&status=${data.status}`
 if (data.status == "STATUS_UNAVAILABLE") {
     return new NextResponse(`<!DOCTYPE html><html><head>
             <title>Success Page</title>
             <meta property="fc:frame" content="vNext" />
-            <meta property="fc:frame:image" content=${imageUrl}/>
+            <meta property="fc:frame:image" content="${imageUrl}"/>
             <meta property="fc:frame:button:1" content="Back" />
             <meta property="fc:frame:button:1:action" content="post"/>
             <meta property="fc:frame:post_url" content="https://3dns-domain-register-frame.vercel.app/api/frame/process"/>
@@ -26,7 +26,7 @@ if (data.status == "STATUS_UNAVAILABLE") {
 return new NextResponse(`<!DOCTYPE html><html><head>
   <title>Success Page</title>
   <meta property="fc:frame" content="vNext" />
-  <meta property="fc:frame:image" content=${imageUrl}/>
+  <meta property="fc:frame:image" content="${imageUrl}"/>
   <meta property="fc:frame:button:1" content="Back" />
   <meta property="fc:frame:button:1:action" content="post"/>
   <meta property="fc:frame:button:2" content="Purchase" />
