e970cb835ae73026b976ebdbfbf9afb01c37cad5
S29 <nots29vv@gmail.com>
2024-03-04 13:36:19 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import { redirect } from 'next/navigation'
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
+const REDIRECT_URL = process.env.REDIRECT_URL || "https://app.3dns.box"
+const FRAMES_URL = process.env.FRAMES_URL || "https://3dns-domain-register-frame.vercel.app"
+const imageUrl = new URL("/3dns-buy-a-domain-rescale.png", FRAMES_URL).href
+const postUrl = new URL("/api/frame", FRAMES_URL).href
 
 const frameMetadata = getFrameMetadata({
   buttons: [
       {label: 'Buy 🛍️', action: 'post'},
   ],
-  image: 'https://3dns-domain-register-frame.vercel.app/3dns-buy-a-domain-rescale.png',
-  post_url: 'https://3dns-domain-register-frame.vercel.app/api/frame',
+  image: imageUrl,
+  post_url: postUrl,
 
 });
 
@@ -18,7 +22,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: '3 DNS Frame by 3DNS.',
     description: 'A frame to search if a domain exists, and to also purchase it via 3dns.',
-    images: [`https://3dns-domain-register-frame.vercel.app/3dns-buy-a-domain-rescale.png`],
+    images: [imageUrl],
   },
   other: {
     ...frameMetadata,
@@ -26,17 +30,9 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-    const redirectUrl = `https://app.3dns.box`;
-
+    const redirectUrl = REDIRECT_URL;
     redirect(redirectUrl)
 }
-
-
-// ref: https://www.pinata.cloud/blog/how-to-make-a-frame-on-farcaster-using-ipfs
-// spec: https://docs.farcaster.xyz/reference/frames/spec
-// playground: https://og-playground.vercel.app/
-
-
 
 
 
