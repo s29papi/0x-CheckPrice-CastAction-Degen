3f7a517053f1225c479130f2c94c01f22e71ddab
S29 <nots29vv@gmail.com>
2024-05-02 23:24:47 +0100

    third commit

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,67 +1,42 @@
 'use client';
-// 'use client';
+
 import {useRouter, useSearchParams} from "next/navigation";
 import {useEffect} from "react";
+import { Suspense } from 'react'
 
 export default function RedirectPage() {
     const searchParams = useSearchParams();
     const router = useRouter();
 
-    const txhash = searchParams.get('txhash')
-
-    useEffect(() => {
-        const baseUrl = `https://basescan.org/tx/${txhash}`;
-
-        // Perform the redirect
-        window.location.href = baseUrl;
-    }, [router, txhash]);
-
-    return (
-        <div>
-            <p>Redirecting...</p>
-        </div>
-    );
-}
-
-
-
-// import {useRouter, useSearchParams} from "next/navigation";
-// import {useEffect} from "react";
-// import { Suspense } from 'react'
-
-// export default function RedirectPage() {
-//     const searchParams = useSearchParams();
-    // const router = useRouter();
-
-    // const fid = searchParams.get('fid');
-    // const buttonIdx = searchParams.get('buttonIdx');
+    const fid = searchParams.get('fid');
+    const buttonIdx = searchParams.get('buttonIdx');
 
 
     
 
-    // useEffect(() => {
-    //     if (buttonIdx == "1") {
-    //         (async () => {
-    //             const trendingUrl = "https://drakula.app/trending";
+    useEffect(() => {
+        if (buttonIdx == "1") {
+            (async () => {
+                const trendingUrl = "https://drakula.app/trending";
 
             
-    //             window.location.href = trendingUrl;
-    //         })()
-    //     }
+                window.location.href = trendingUrl;
+            })()
+        }
 
-    //     if (buttonIdx == "2") {
-    //         const trendingUrl = "https://drakula.app/trending";
+        if (buttonIdx == "2") {
+            const trendingUrl = "https://drakula.app/trending";
 
             
-    //         window.location.href = trendingUrl;
-    //     }
-    // }, [router, fid, buttonIdx]);
+            window.location.href = trendingUrl;
+        }
+    }, [router, fid, buttonIdx]);
 
-//     return (
-//         <Suspense>
-//             <div>
-//                  <p>Redirecting...</p>
-//             </div>
-//         </Suspense>
-//     );
-// }
+    return (
+        <Suspense>
+            <div>
+                 <p>Redirecting...</p>
+            </div>
+        </Suspense>
+    );
+}
