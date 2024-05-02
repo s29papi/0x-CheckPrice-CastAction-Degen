8c497624c1edc7c722ee00919c74052bc9aec64d
S29 <nots29vv@gmail.com>
2024-05-02 13:59:17 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
 const REDIRECT_URL = process.env.REDIRECT_URL || "https://drakula.app"
-const FRAMES_URL = process.env.FRAMES_URL || "https://3dns-domain-register-frame.vercel.app"
+const FRAMES_URL = process.env.FRAMES_URL || "https://drakula-view.vercel.app"
 const imageUrl = new URL("/main.png", FRAMES_URL).href
 const postUrl = new URL("/api/frame", FRAMES_URL).href
 
