1ceda767b7801ae849fccd257dc7c0a207007a71
S29 <nots29vv@gmail.com>
2024-05-03 08:41:12 +0100

    modified api and modules

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -25,10 +25,10 @@ async function ogImgWithStatement(statement: string): ImageResponse {
                 <div tw="flex flex-col w-full h-full absolute">
                         <div tw="flex h-full w-full">
                             <div tw="flex flex-col md:flex-row w-full py-12 px-4 justify-between p-8">
-                                <div style={{display: 'flex', position: 'absolute', top: '80%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1400'}}> 
+                                <div style={{display: 'flex', position: 'absolute', top: '84%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1400'}}> 
                                         <span tw="flex flex-col text-lg sm:text-xl md:flex-row w-full py-12 px-4 justify-between p-8">
                                                 <span tw="flex flex-col bottom-7">
-                                                <span tw="text-3xl top-[15.95] left-[10] text-white">{statement}</span>
+                                                <span tw="text-4xl top-[15.95] left-[10] text-white">{statement}</span>
                                                 </span>  
                                         </span> 
                                 </div>
