1284df54ea3f97f150fde6fdc1f53a6a6c59e3a4
S29 <nots29vv@gmail.com>
2024-05-11 15:43:53 +0100

    price and buy og

---
+++ app/api/approve-buyTx/route.ts
@@ -0,0 +1,65 @@
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
+import { encodeFunctionData, formatEther, parseEther, parseUnits  } from 'viem';
+import { base } from 'viem/chains';
+import type { FrameTransactionResponse } from '@coinbase/onchainkit/frame';
+import { getFarcasterUserAddress } from '@coinbase/onchainkit/farcaster';
+import HIGHERABI from "../../utils/higher-abi"
+import { HIGHER_BASE_CONTRACT_ADDR } from '@/app/utils/addrs';
+
+
+async function getResponse(req: NextRequest): Promise<NextResponse | Response> { 
+    const body: FrameRequest = await req.json();
+    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+    if (!isValid) {
+        return new NextResponse('Message not valid', { status: 500 });
+      }
+
+    // if (body.untrustedData.fid) {
+    //     const farcasterUserAddress = await getFarcasterUserAddress(body.untrustedData.fid, {
+    //         neynarApiKey: 'NEYNAR_ONCHAIN_KIT', 
+    //       });
+    // }
+
+    let option = {
+        method: 'GET',
+        headers: {accept: 'application/json', '0x-api-key': `${process.env.NEXT_PUBLIC_OX_KEY}`}
+    }
+
+    let sellToken_usdc = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'
+    let buyToken_higher = '0x0578d8A44db98B23BF096A382e016e29a5Ce0ffe'
+    let usdc_amount = parseUnits(body.untrustedData.inputText, 6) 
+    let url = `https://base.api.0x.org/swap/v1/quote?sellToken=${sellToken_usdc}&buyToken=${buyToken_higher}&sellAmount=${usdc_amount}`
+    const response = await fetch(url, option);
+    const jsonData = await response.json();
+
+    
+    let data = encodeFunctionData({
+        abi: HIGHERABI,
+        functionName: 'approve',
+        args: [ `0x${jsonData.allowanceTarget.slice(2)}`, parseUnits(body.untrustedData.inputText, 6) ],
+    })
+
+    
+    const txData: FrameTransactionResponse = {
+        chainId: `eip155:${base.id}`,
+        method: 'eth_sendTransaction',
+        params: {
+            abi: [],
+            data,
+            to: `0x${sellToken_usdc.slice(2)}`,
+            value: parseEther('0.00000').toString(),
+        },
+    };
+    
+    return NextResponse.json(txData);
+
+}
+
+
+export async function POST(req: NextRequest): Promise<Response> {
+    return getResponse(req);
+  }
+  
+  export const dynamic = 'force-dynamic';
--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -5,8 +5,9 @@ import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit';
 export async function POST(req: NextRequest) {
     const body: FrameRequest = await req.json();
     
-    let postUrl = "https://drakula-view.vercel.app/api/frame/pre-redirect";
+    let postUrl = "https://0x-check-price-cast-action.vercel.app";
     let imageUrl = "https://0x-check-price-cast-action.vercel.app/og/price-and-buy";
+    let buytxUrl = "https://0x-check-price-cast-action.vercel.app/api/buy-tx"
     
 
 
@@ -18,8 +19,12 @@ export async function POST(req: NextRequest) {
             <meta property="og:image" content="${imageUrl}"/>
             <meta property="fc:frame:image" content="${imageUrl}"/>
             <meta property="fc:frame:input:text" content="Enter Amount $HIGHER"/>
-            <meta property="fc:frame:button:1" content="Buy" />
-            <meta property="fc:frame:button:1:action" content="post_redirect"/>
+            <meta property="fc:frame:button:1" content="Approve Buy From 0x ✅" />
+            <meta property="fc:frame:button:1:action" content="tx"/>
+            <meta property="fc:frame:button:1:target" content="${buytxUrl}"/>
+            <meta property="fc:frame:button:2" content="Buy 🚀" />
+            <meta property="fc:frame:button:2:action" content="tx"/>
+            <meta property="fc:frame:button:2:target" content="${buytxUrl}"/>
             <meta property="fc:frame:post_url" content="${postUrl}"/>
             </head></html>`,
       {
